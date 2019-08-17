2985276a7dad3514c05a9277a632058a91cb871c
theEvilReaper <steffenwx@gmail.com>
2019-08-17 23:43:29 +0200

    Add method to receive a random player

--- src/main/java/de/cosmiqglow/aves/util/PlayerUtil.java
+++ src/main/java/de/cosmiqglow/aves/util/PlayerUtil.java
@@ -2,7 +2,10 @@ package de.cosmiqglow.aves.util;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import java.util.Optional;
 
 /**
  * Contains some methods to work with {@link Player} objects
@@ -17,5 +20,15 @@ public class PlayerUtil {
 
     public static void sendActionBar(Player player, String message) {
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
+    }
+
+    /**
+     * Choose a random player from all players which are currently online
+     * @return a random player
+     */
+
+    public static Optional<Player> getRandomPlayer() {
+        return Optional.ofNullable(Bukkit.getOnlinePlayers().stream().skip((int)
+                (Bukkit.getOnlinePlayers().size() * Math.random())).findFirst().orElse(null));
     }
 }
