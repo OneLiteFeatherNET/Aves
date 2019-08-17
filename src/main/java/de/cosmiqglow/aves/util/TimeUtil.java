0e98bfa6d0bec608f53ce9f107495537228ec51b
theEvilReaper <steffenwx@gmail.com>
2019-08-17 15:40:03 +0200

    Add TimeUtil

---
+++ src/main/java/de/cosmiqglow/aves/util/TimeUtil.java
@@ -0,0 +1,21 @@
+package de.cosmiqglow.aves.util;
+
+import com.google.common.base.Joiner;
+
+public class TimeUtil {
+
+    /**
+     * Convert a time value into a hh:mm format
+     * @param time The time who should be converted
+     * @return The converted time
+     */
+
+    public static String getTimeString(final int time) {
+        if (time <= 0) {
+            return "00:00";
+        }
+        int minutes = time / 60;
+        int seconds = time % 60;
+        return Joiner.on(":").join((minutes < 10) ? "0" + minutes : minutes, (seconds < 10) ? "0" + seconds : seconds);
+    }
+}
