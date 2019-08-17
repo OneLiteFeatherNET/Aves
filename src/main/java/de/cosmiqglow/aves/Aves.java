6d96d648e4625a340a62bf9bae8d3396f9553aa4
theEvilReaper <steffenwx@gmail.com>
2019-08-17 15:41:45 +0200

    Add load logic

--- src/main/java/de/cosmiqglow/aves/Aves.java
+++ src/main/java/de/cosmiqglow/aves/Aves.java
@@ -1,4 +1,24 @@
 package de.cosmiqglow.aves;
 
-public class Aves  {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Aves extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println(
+                "    /\\                 \n" +
+                        "   /  \\__   _____  ___ \n" +
+                        "  / /\\ \\ \\ / / _ \\/ __|\n" +
+                        " / ____ \\ V /  __/\\__ \\\n" +
+                        "/_/    \\_\\_/ \\___||___/");
+        System.out.println(String.format("Starting Aves v%s by %s",
+                getDescription().getVersion(), getDescription().getAuthors()));
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
 }
