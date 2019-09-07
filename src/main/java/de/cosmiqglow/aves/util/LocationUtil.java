7b58616a870f4aee61acac7f1b84ec6389b5f64b
Rxcki <patrick.zdarsky@gmail.com>
2019-09-07 21:10:16 +0200

    NET-32: Add Util Location Compare Function

--- src/main/java/de/cosmiqglow/aves/util/LocationUtil.java
+++ src/main/java/de/cosmiqglow/aves/util/LocationUtil.java
@@ -8,4 +8,8 @@ public class LocationUtil {
     public static Location getCenter(Block block) {
         return block.getLocation().add(0.5, 0, 0.5);
     }
+
+    public static boolean Compare(Location location1, Location location2, double delta) {
+        return location1.distanceSquared(location2) < Math.pow(delta, 2);
+    }
 }
