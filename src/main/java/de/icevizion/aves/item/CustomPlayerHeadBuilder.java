97e8f39ef970a8fa1ad54e5ba0ec8454fb0ec90f
theEvilReaper <steffenwx@gmail.com>
2020-03-19 21:17:22 +0100

    Small improvements

--- src/main/java/de/icevizion/aves/item/CustomPlayerHeadBuilder.java
+++ src/main/java/de/icevizion/aves/item/CustomPlayerHeadBuilder.java
@@ -3,6 +3,8 @@ package de.icevizion.aves.item;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import java.lang.reflect.Field;
@@ -13,6 +15,14 @@ public final class CustomPlayerHeadBuilder extends ItemBuilder {
 
     public CustomPlayerHeadBuilder() {
         super(Material.SKULL_ITEM);
+    }
+
+    public CustomPlayerHeadBuilder setSkullType(SkullType type) {
+        ItemMeta stackMeta = stack.getItemMeta();
+        ItemStack item = new ItemStack(stack.getType(), stack.getAmount(), (short) type.getData());
+        item.setItemMeta(stackMeta);
+        this.stack = item;
+        return this;
     }
 
     /**
@@ -60,5 +70,24 @@ public final class CustomPlayerHeadBuilder extends ItemBuilder {
     @Override
     protected SkullMeta getItemMeta() {
         return (SkullMeta) super.getItemMeta();
+    }
+
+    public enum SkullType {
+
+        SKELETON(0),
+        SKELETON_WITHER(1),
+        ZOMBIE(2),
+        PLAYER(3),
+        CREEPER(4);
+
+        private int data;
+
+        SkullType(int data) {
+            this.data = data;
+        }
+
+        public int getData() {
+            return data;
+        }
     }
 }
