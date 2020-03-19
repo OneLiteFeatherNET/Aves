7828fe2d9b44eb3bba21e859e05565a1c12d085b
theEvilReaper <steffenwx@gmail.com>
2020-03-19 21:16:16 +0100

    Add builder for spawn eggs

---
+++ src/main/java/de/icevizion/aves/item/SpawnEggBuilder.java
@@ -0,0 +1,18 @@
+package de.icevizion.aves.item;
+
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.material.SpawnEgg;
+
+public final class SpawnEggBuilder extends ItemBuilder {
+
+    public SpawnEggBuilder() {
+        super(Material.MONSTER_EGG);
+    }
+
+    public SpawnEggBuilder setType(EntityType type) {
+        SpawnEgg egg = new SpawnEgg(type);
+        stack.setData(egg.toItemStack().getData());
+        return this;
+    }
+}
