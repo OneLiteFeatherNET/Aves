c9b5821aaa9a004f6882948d862857b10618b0bc
theEvilReaper <steffenwx@gmail.com>
2020-03-27 14:18:13 +0100

    A class was added the auxiliary methods contained for blocks

---
+++ src/main/java/de/icevizion/aves/util/BlockUtil.java
@@ -0,0 +1,28 @@
+package de.icevizion.aves.util;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.Set;
+
+public class BlockUtil {
+
+    /**
+     * Gets the BlockFace of the block the player is currently targeting.
+     *
+     * @param player the player's whos targeted blocks BlockFace is to be checked.
+     * @param distance the distance to scan
+     * @return the BlockFace of the targeted block, or null if the targeted block is non-occluding.
+     */
+
+    public static BlockFace getBlockFace(Player player, int distance) {
+        if (distance > 100) {
+            throw new IllegalArgumentException("The distance to scan must between 0 and 100");
+        }
+        List<Block> lastTwoTargetBlocks = player.getLastTwoTargetBlocks((Set<Material>) null, distance);
+        return lastTwoTargetBlocks.get(1).getFace(lastTwoTargetBlocks.get(0));
+    }
+}
