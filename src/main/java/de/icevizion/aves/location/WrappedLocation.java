c6f34d2bb14ff56990e126dde951ee53292f69e6
theEvilReaper <steffenwx@gmail.com>
2020-04-04 11:48:31 +0200

    Improve toLocation method and add some documentation

--- src/main/java/de/icevizion/aves/location/WrappedLocation.java
+++ src/main/java/de/icevizion/aves/location/WrappedLocation.java
@@ -20,6 +20,14 @@ public class WrappedLocation {
 
     private transient Location location;
 
+    /**
+     * Creates a new object from the wrapper with the given values.
+     * @param world The world name as string
+     * @param x The x coordinate
+     * @param y The y coordinate
+     * @param z The z coordinate
+     */
+
     public WrappedLocation(String world, double x, double y, double z) {
         this.world = world;
         this.x = x;
@@ -28,6 +36,16 @@ public class WrappedLocation {
         this.yaw = 0;
         this.pitch = 0;
     }
+
+    /**
+     * Creates a new object from the wrapper with the given values.
+     * @param world The world name as string
+     * @param x The x coordinate
+     * @param y The y coordinate
+     * @param z The z coordinate
+     * @param yaw The yaw value
+     * @param pitch The pitch value
+     */
 
     public WrappedLocation(String world, double x, double y, double z, float yaw, float pitch) {
         this.world = world;
@@ -38,25 +56,55 @@ public class WrappedLocation {
         this.pitch = pitch;
     }
 
+    /**
+     * Give the wrapper a world name.
+     * @param world The world name to set
+     */
+
     public void setWorld(String world) {
         this.world = world;
     }
+
+    /**
+     * Give the wrapper the x coordinate.
+     * @param x The x coordinate to set
+     */
 
     public void setX(double x) {
         this.x = x;
     }
 
+    /**
+     * Give the wrapper the y coordinate.
+     * @param y The y coordinate to set
+     */
+
     public void setY(double y) {
         this.y = y;
     }
+
+    /**
+     * Give the wrapper the z coordinate.
+     * @param z The z coordinate to set
+     */
 
     public void setZ(double z) {
         this.z = z;
     }
 
+    /**
+     * Give the wrapper the yaw value.
+     * @param yaw The yaw value to set
+     */
+
     public void setYaw(float yaw) {
         this.yaw = yaw;
     }
+
+    /**
+     * Give the wrapper the pitch value.
+     * @param pitch The pitch value to set
+     */
 
     public void setPitch(float pitch) {
         this.pitch = pitch;
@@ -93,7 +141,7 @@ public class WrappedLocation {
         }
 
         if (location != null) {
-            Location location = new Location(bukkitWorld, x,y,z);
+            location = new Location(bukkitWorld, x,y,z);
             location.setYaw(yaw);
             location.setPitch(pitch);
         }
