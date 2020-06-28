9eaf0fe49c57685a64dadab54ecb8abb87f6db59
theEvilReaper <steffenwx@gmail.com>
2020-06-28 16:22:54 +0200

    Marked some methods as deprecated

--- src/main/java/de/icevizion/aves/util/LocationUtil.java
+++ src/main/java/de/icevizion/aves/util/LocationUtil.java
@@ -1,6 +1,7 @@
 package de.icevizion.aves.util;
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 
 /**
  * The class contains some methods that work with {@link Location}
@@ -9,28 +10,29 @@ public class LocationUtil {
 
     /**
      * Returns the center of the block.
-     * @param location The block where the center should be calculated from
+     * @param block The block where the center should be calculated from
      * @return The middle location of the given block
      */
 
-    public static Location getCenter(Location location) {
-        return location.add(0.5, 0, 0.5);
+    @Deprecated
+    public static Location getCenter(Block block) {
+        return block.getLocation().add(0.5, 0, 0.5);
     }
 
     /**
      * Returns the center of the block over 3 dimensions
-     * @param location The block where the center should be calculated from
+     * @param block The block where the center should be calculated from
      * @return The middle location of the given block
      */
-
-    public static Location getCenter3D(Location location) { return location.add(0.5, -0.5, 0.5); }
+    @Deprecated
+    public static Location getCenter3D(Block block) { return block.getLocation().add(0.5, -0.5, 0.5); }
 
     /**
      * Compare two locations with the given delta.
      * @param location1 The first location
      * @param location2 The second location
      * @param delta The maximum delta that is allowed between both locations
-     * @return True, if the location between both locations is less than the given delta, false otherwise
+     * @return True, if the location is the same with the given delta
      */
 
     public static boolean compare(Location location1, Location location2, double delta) {
@@ -39,17 +41,20 @@ public class LocationUtil {
         return location1.distanceSquared(location2) <= Math.pow(delta, 2);
     }
 
+    /**
+     * Compare two locations if they are the same, the angle can also be considered.
+     * @param location1 The first location
+     * @param location2 The second location
+     * @param angle Whether the angle should be included
+     * @return True, if the location between both locations is less than the given delta, false otherwise
+     */
+
     public static boolean compare(Location location1, Location location2, boolean angle) {
-        boolean position =  location1.getX() == location2.getX()
+        boolean position = location1.getX() == location2.getX()
                 && location1.getY() == location2.getY()
                 && location1.getZ() == location2.getZ();
-
-        if (!position)
-            return false;
-
         if (angle) {
-            return location1.getYaw() == location2.getYaw()
-                    && location1.getPitch() == location2.getPitch();
+            position = location1.getYaw() == location2.getYaw() && location1.getPitch() == location2.getPitch();
         }
         return position;
     }
