4e03b8d4050653f294922325ef894499e49c45bd
theEvilReaper <steffenwx@gmail.com>
2020-07-17 18:47:41 +0200

    Changed io backend to nio

--- src/main/java/de/icevizion/aves/file/JsonFileLoader.java
+++ src/main/java/de/icevizion/aves/file/JsonFileLoader.java
@@ -1,33 +1,42 @@
 package de.icevizion.aves.file;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
 public final class JsonFileLoader {
 
-    public static <T> void save(File file, T object, Gson gson) {
-        try (FileWriter fw = new FileWriter(file)) {
-            if (!file.exists())
-                if (file.createNewFile())
-                   System.out.println("Created new file: " + file.getName());
+    /**
+     * Saves a given object into a file.
+     * @param path The path where the file is located
+     * @param object The object to save
+     * @param gson The gson instance
+     * @param <T> A generic type for the object value
+     */
 
-            String json = gson.toJson(object);
-            fw.write(json);
-        } catch (Exception e) {
+    public static <T> void save(Path path, T object, Gson gson) {
+        try (BufferedWriter fw = Files.newBufferedWriter(path)) {
+            if (!Files.exists(path)) {
+                System.out.println("Created new file: " + Files.createFile(path).getFileName().toString());
+            }
+            gson.toJson(object, TypeToken.get(object.getClass()).getType(), fw);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static <T> Optional<T> load(File file, Class<T> clazz, Gson gson) {
-        if (!file.exists())
+    public static <T> Optional<T> load(Path path, Class<T> clazz, Gson gson) {
+        if (!Files.exists(path))
             return Optional.empty();
 
-        try (FileReader fr = new FileReader(file)) {
+        try (BufferedReader fr = Files.newBufferedReader(path)) {
             return Optional.of(gson.fromJson(fr, clazz));
         } catch (Exception e) {
             e.printStackTrace();
@@ -35,11 +44,11 @@ public final class JsonFileLoader {
         }
     }
 
-    public static <T> Optional<T> load(File file, Type type, Gson gson) {
-        if (!file.exists())
+    public static <T> Optional<T> load(Path path, Type type, Gson gson) {
+        if (!Files.exists(path))
             return Optional.empty();
 
-        try (FileReader fr = new FileReader(file)) {
+        try (BufferedReader fr = Files.newBufferedReader(path)) {
             return Optional.of(gson.fromJson(fr, type));
         } catch (Exception e) {
             e.printStackTrace();
