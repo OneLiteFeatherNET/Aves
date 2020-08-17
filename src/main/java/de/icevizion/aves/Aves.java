85790cd470de9d6766cf2e4d82b1385c989df5f0
theEvilReaper <steffenwx@gmail.com>
2020-08-17 14:38:51 +0200

    Small improvements

--- src/main/java/de/icevizion/aves/Aves.java
+++ src/main/java/de/icevizion/aves/Aves.java
@@ -12,8 +12,8 @@ public final class Aves extends JavaPlugin {
                 "  / /\\ \\ \\ / / _ \\/ __|\n" +
                 " / ____ \\ V /  __/\\__ \\\n" +
                 "/_/    \\_\\_/ \\___||___/");
-        System.out.println(String.format("Starting Aves v%s by %s",
-                getDescription().getVersion(), getDescription().getAuthors()));
+        System.out.printf("Starting Aves v%s by %s%n",
+                getDescription().getVersion(), getDescription().getAuthors());
     }
 
     @Override
