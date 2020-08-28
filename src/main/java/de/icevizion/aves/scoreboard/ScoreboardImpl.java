10cd7ee566125bdb06830674093ad5e4112c6e9b
JumpingPxl <jumpingpxl@gmail.com>
2020-08-28 18:45:18 +0200

    Add Scoreboard Builder

---
+++ src/main/java/de/icevizion/aves/scoreboard/ScoreboardImpl.java
@@ -0,0 +1,150 @@
+package de.icevizion.aves.scoreboard;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import net.titan.cloudcore.task.Task;
+import net.titan.spigot.player.CloudPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * @author Nico (JumpingPxl) Middendorf
+ */
+
+public class ScoreboardImpl {
+
+	private final Scoreboard scoreboard;
+	private final CloudPlayer cloudPlayer;
+	private final Set<Task> tasks;
+	private final Map<Integer, ScoreboardLine> rows;
+	private final Map<String, ScoreboardObjective> objectives;
+	private final Map<String, ScoreboardBuilder> cachedTranslations;
+
+	/**
+	 * Instantiates a new Scoreboard.
+	 *
+	 * @param scoreboard  the scoreboard
+	 * @param cloudPlayer the cloud player
+	 */
+	protected ScoreboardImpl(Scoreboard scoreboard, CloudPlayer cloudPlayer) {
+		this.scoreboard = scoreboard;
+		this.cloudPlayer = cloudPlayer;
+
+		tasks = Sets.newHashSet();
+		rows = Maps.newHashMap();
+		objectives = Maps.newHashMap();
+		cachedTranslations = Maps.newHashMap();
+	}
+
+	/**
+	 * Gets bukkit scoreboard.
+	 *
+	 * @return the bukkit scoreboard
+	 */
+	public Scoreboard getBukkitScoreboard() {
+		return scoreboard;
+	}
+
+	/**
+	 * Gets team by the team name and creates one by the given name if the team isn't existing yet.
+	 *
+	 * @param teamName the team name
+	 * @return the team
+	 */
+	public Team getTeam(Object teamName) {
+		Optional<Team> optionalTeam = getOptionalTeam(teamName);
+		return optionalTeam.orElseGet(() -> scoreboard.registerNewTeam(teamName.toString()));
+	}
+
+	/**
+	 * Gets optional team by the team name. Returns an empty optional if the team doesn't exist.
+	 *
+	 * @param teamName the team name
+	 * @return the optional team
+	 */
+	public Optional<Team> getOptionalTeam(Object teamName) {
+		if (!teamExists(teamName)) {
+			return Optional.empty();
+		}
+
+		Team team = scoreboard.getTeam(teamName.toString());
+		return Optional.of(team);
+	}
+
+	/**
+	 * Checks if a team exists by the team name.
+	 *
+	 * @param teamName the team name
+	 * @return the boolean
+	 */
+	public boolean teamExists(Object teamName) {
+		Team team = scoreboard.getTeam(teamName.toString());
+		return Objects.nonNull(team);
+	}
+
+	/**
+	 * Sets the scoreboard.
+	 */
+	public void setScoreboard() {
+		cloudPlayer.getPlayer().setScoreboard(scoreboard);
+	}
+
+	/**
+	 * Unset current scoreboard by using the servers main scoreboard.
+	 */
+	public void unsetScoreboard() {
+		cloudPlayer.getPlayer().setScoreboard(Bukkit.getScoreboardManager().getMainScoreboard());
+	}
+
+	/**
+	 * Gets the cloud player.
+	 *
+	 * @return the cloud player
+	 */
+	protected CloudPlayer getCloudPlayer() {
+		return cloudPlayer;
+	}
+
+	/**
+	 * Gets tasks.
+	 *
+	 * @return the tasks
+	 */
+	protected Set<Task> getTasks() {
+		return tasks;
+	}
+
+	/**
+	 * Gets rows.
+	 *
+	 * @return the rows
+	 */
+	protected Map<Integer, ScoreboardLine> getRows() {
+		return rows;
+	}
+
+	/**
+	 * Gets objectives.
+	 *
+	 * @return the objectives
+	 */
+	protected Map<String, ScoreboardObjective> getObjectives() {
+		return objectives;
+	}
+
+	/**
+	 * Gets cached translations.
+	 *
+	 * @return the cached translations
+	 */
+	protected Map<String, ScoreboardBuilder> getCachedTranslations() {
+		return cachedTranslations;
+	}
+}
+
---
+++ src/main/java/de/icevizion/aves/scoreboard/ScoreboardObjective.java
@@ -0,0 +1,115 @@
+package de.icevizion.aves.scoreboard;
+
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+
+import java.util.Objects;
+
+/**
+ * @author Nico (JumpingPxl) Middendorf
+ */
+
+public class ScoreboardObjective {
+
+	private final ScoreboardBuilder scoreboard;
+	private final Objective objective;
+
+	/**
+	 * Instantiates a new Scoreboard objective.
+	 *
+	 * @param scoreboardBuilder the scoreboard builder
+	 * @param objectiveName     the objective name
+	 */
+	protected ScoreboardObjective(ScoreboardBuilder scoreboardBuilder, String objectiveName) {
+		this.scoreboard = scoreboardBuilder;
+
+		Objective currentObjective = scoreboardBuilder.getBukkitScoreboard().getObjective(
+				objectiveName);
+		if (Objects.isNull(currentObjective)) {
+			objective = scoreboardBuilder.getBukkitScoreboard().registerNewObjective(objectiveName,
+					"dummy");
+		} else {
+			objective = currentObjective;
+		}
+	}
+
+	/**
+	 * Gets the Bukkit objective.
+	 *
+	 * @return the objective
+	 */
+	public Objective getObjective() {
+		return objective;
+	}
+
+	/**
+	 * Gets the display name.
+	 *
+	 * @return the display name
+	 */
+	public String getDisplayName() {
+		return objective.getDisplayName();
+	}
+
+	/**
+	 * Gets the display slot.
+	 *
+	 * @return the display slot
+	 */
+	public DisplaySlot getDisplaySlot() {
+		return objective.getDisplaySlot();
+	}
+
+	/**
+	 * Gets score.
+	 *
+	 * @param entry the entry
+	 * @return the score
+	 */
+	public Score getScore(String entry) {
+		return objective.getScore(entry);
+	}
+
+	/**
+	 * Go back to the scoreboard builder.
+	 *
+	 * @return the scoreboard builder
+	 */
+	public ScoreboardBuilder apply() {
+		return scoreboard;
+	}
+
+	/**
+	 * Sets display name of the objective.
+	 *
+	 * @param displayName the display name
+	 * @return the display name
+	 */
+	public ScoreboardObjective setDisplayName(String displayName) {
+		if (displayName.length() > 16) {
+			displayName = displayName.substring(0, 16);
+		}
+
+		objective.setDisplayName(displayName);
+		return this;
+	}
+
+	/**
+	 * Sets display slot of the objective.
+	 *
+	 * @param displaySlot the display slot
+	 * @return the display slot
+	 */
+	public ScoreboardObjective setDisplaySlot(DisplaySlot displaySlot) {
+		objective.setDisplaySlot(displaySlot);
+		return this;
+	}
+
+	/**
+	 * Unregister the current objective.
+	 */
+	public void unregister() {
+		objective.unregister();
+	}
+}
