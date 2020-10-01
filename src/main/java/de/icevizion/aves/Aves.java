91340ac7a6340baadc7900dc612e52ed8113bde3
theEvilReaper <steffenwx@gmail.com>
2020-10-01 18:51:23 +0200

    Small improvements

--- src/main/java/de/icevizion/aves/Aves.java
+++ src/main/java/de/icevizion/aves/Aves.java
@@ -24,29 +24,29 @@ public final class Aves extends Plugin {
         System.out.printf("Starting Aves v%s by %s%n",
             getDescription().getVersion(), getDescription().getAuthors());
 
-      CloudService cloudService = getService(CloudService.class);
+        CloudService cloudService = getService(CloudService.class);
 
       //Inventory Builder
 	    InventoryService inventoryService = new InventoryService();
-      addService(inventoryService);
+        addService(inventoryService);
 
-	    InventoryBuilderListener inventoryListener = new InventoryBuilderListener(cloudService);
+        InventoryBuilderListener inventoryListener = new InventoryBuilderListener(cloudService);
 
-      getServer().getPluginManager().registerEvents(
-          new InventoryBuilderClickListener(inventoryListener), this);
-      getServer().getPluginManager().registerEvents(
-          new InventoryBuilderCloseListener(inventoryListener), this);
-      getServer().getPluginManager().registerEvents(
-          new InventoryBuilderDragListener(inventoryListener), this);
+        getServer().getPluginManager().registerEvents(
+                new InventoryBuilderClickListener(inventoryListener), this);
+        getServer().getPluginManager().registerEvents(
+                new InventoryBuilderCloseListener(inventoryListener), this);
+        getServer().getPluginManager().registerEvents(
+                new InventoryBuilderDragListener(inventoryListener), this);
 
-      //Name Tags
-      NameTagService nameTagService = new NameTagService(cloudService);
-      addService(nameTagService);
+        //Name Tags
+        NameTagService nameTagService = new NameTagService(cloudService);
+        addService(nameTagService);
 
-      getServer().getPluginManager().registerEvents(
-          new NameTagPlayerJoinListener(nameTagService), this);
-      getServer().getPluginManager().registerEvents(
-          new NameTagPlayerQuitListener(nameTagService), this);
+        getServer().getPluginManager().registerEvents(
+                new NameTagPlayerJoinListener(nameTagService), this);
+        getServer().getPluginManager().registerEvents(
+                new NameTagPlayerQuitListener(nameTagService), this);
     }
 
     @Override
