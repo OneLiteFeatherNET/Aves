26ccf18b5f9c149f211f6f85e6a10603d2732c8d
theEvilReaper <steffenwx@gmail.com>
2020-10-31 15:48:58 +0100

    Renamed class and removed some reflection method

---
+++ src/main/java/de/icevizion/aves/reflection/ServerHelper.java
@@ -0,0 +1,21 @@
+package de.icevizion.aves.reflection;
+
+import org.bukkit.Bukkit;
+
+/**
+ * @author Patrick Zdarsky / Rxcki
+ * @version 1.0
+ * @since 18/04/2020 15:44
+ */
+
+public class ServerHelper {
+
+    /**
+     * Returns the current TPS from the server.
+     * @return The TPS values as double
+     */
+
+    public static double[] getTps() {
+        return Bukkit.getTPS();
+    }
+}
