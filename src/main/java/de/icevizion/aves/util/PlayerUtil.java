fa2044628e3461510912fc0741d57087de96fb1e
theEvilReaper <steffenwx@gmail.com>
2020-11-01 20:49:41 +0100

    Added method to drop an amount of items

--- src/main/java/de/icevizion/aves/util/PlayerUtil.java
+++ src/main/java/de/icevizion/aves/util/PlayerUtil.java
@@ -3,10 +3,14 @@ package de.icevizion.aves.util;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -15,6 +19,58 @@ import java.util.stream.Collectors;
  * Contains some methods to work with {@link Player} objects
  */
 public class PlayerUtil {
+
+    /**
+     * Drops the complete inventory content from a player to a specific location.
+     * @param player The player from which the inventory should be dropped
+     */
+
+    public static void dropInventory(Player player) {
+        Objects.requireNonNull(player, "The given player can not be null");
+
+        PlayerInventory playerInventory = player.getInventory();
+
+        if (playerInventory.getContents().length != 0) {
+            dropInventory(player.getLocation(), playerInventory.getContents());
+        }
+
+        if (playerInventory.getArmorContents().length != 0) {
+            dropInventory(player.getLocation(), playerInventory.getArmorContents());
+        }
+
+    }
+
+    /**
+     * Drops a certain amount of items to a given location.
+     * @param location The location where the items should be dropped.
+     * @param content The items stored in a array
+     */
+
+    public static void dropInventory(Location location, ItemStack[] content) {
+        if (content == null) {
+            throw new IllegalArgumentException("The array can not be null");
+        } else {
+            for (int i = 0; i < content.length; i++) {
+                location.getWorld().dropItemNaturally(location, content[i]);
+            }
+        }
+    }
+
+    /**
+     * Drops a certain amount of items to a given location.
+     * @param location The location where the items should be dropped.
+     * @param content The items stored in a list
+     */
+
+    public static void dropInventory(Location location, List<ItemStack> content) {
+        if (content == null || content.isEmpty()) {
+            throw new IllegalArgumentException("The list can not be null or empty");
+        } else {
+            for (int i = 0; i < content.size(); i++) {
+                location.getWorld().dropItem(location, content.get(i));
+            }
+        }
+    }
 
     /**
      * Choose a random player from all players who are currently online.
