6496dd077245d1fb6a6b128faf11a2a3746afc56
theEvilReaper <steffenwx@gmail.com>
2021-01-15 22:53:31 +0100

    Added some documentation

--- src/main/java/de/icevizion/aves/inventory/PersonalInventory.java
+++ src/main/java/de/icevizion/aves/inventory/PersonalInventory.java
@@ -5,11 +5,22 @@ import net.titan.spigot.player.CloudPlayer;
 
 /**
  * @author Nico (JumpingPxl) Middendorf
+ * @version 1.0.0
+ * @since 1.0.6
  */
 
 public abstract class PersonalInventory extends TranslatedInventory {
 
 	private final CloudPlayer cloudPlayer;
+
+	/**
+	 * Creates a new instance from the {@link PersonalInventory}.
+	 * @param translator The instance to a {@link Translator}
+	 * @param cloudPlayer The {@link CloudPlayer} who owns the inventory
+	 * @param rows The amount of rows
+	 * @param titleKey The key for the inventory name
+	 * @param arguments The arguments if the name contains some variables or so
+	 */
 
 	public PersonalInventory(Translator translator, CloudPlayer cloudPlayer, InventoryRows rows,
 	                         String titleKey, Object... arguments) {
@@ -18,6 +29,11 @@ public abstract class PersonalInventory extends TranslatedInventory {
 	}
 
 	public abstract void draw();
+
+	/**
+	 * Returns the {@link CloudPlayer} who owns the inventory.
+	 * @return The underlying player
+	 */
 
 	public final CloudPlayer getCloudPlayer() {
 		return cloudPlayer;
