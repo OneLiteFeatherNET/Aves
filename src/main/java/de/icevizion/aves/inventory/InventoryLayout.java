945ce1ace3bc53f25eb59d46301a56577d8167da
theEvilReaper <steffenwx@gmail.com>
2021-01-17 23:39:44 +0100

    Added some documentation

--- src/main/java/de/icevizion/aves/inventory/InventoryLayout.java
+++ src/main/java/de/icevizion/aves/inventory/InventoryLayout.java
@@ -9,7 +9,10 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 /**
+ * This class represents a layout where all items are located for an inventory.
  * @author Nico (JumpingPxl) Middendorf
+ * @version 1.0.0
+ * @since 1.0.6
  */
 
 public class InventoryLayout {
@@ -22,20 +25,43 @@ public class InventoryLayout {
 		clickEvents = Maps.newHashMap();
 	}
 
-	public Map<Integer, ItemBuilder> getItems() {
-		return ImmutableMap.copyOf(items);
-	}
-
-	public Map<Integer, Consumer<ClickEvent>> getClickEvents() {
-		return ImmutableMap.copyOf(clickEvents);
-	}
+	/**
+	 * Add an item with an given slot to the layout with an {@link ClickEvent}.
+	 * @param slot The slot id for the item
+	 * @param itemBuilder The inventory builder for the item
+	 * @param clickEvent The {@link ClickEvent} for the item
+	 */
 
 	public void setItem(int slot, ItemBuilder itemBuilder, Consumer<ClickEvent> clickEvent) {
 		setItem(slot, itemBuilder);
 		clickEvents.put(slot, clickEvent);
 	}
 
+	/**
+	 * Add an item with an given slot to the layout.
+	 * @param slot The slot id for the item
+	 * @param itemBuilder The inventory builder for the item
+	 */
+
 	public void setItem(int slot, ItemBuilder itemBuilder) {
 		items.put(slot, itemBuilder);
+	}
+
+	/**
+	 * Returns all registered {@link ItemBuilder} from the layout
+	 * @return the underlying map
+	 */
+
+	public Map<Integer, ItemBuilder> getItems() {
+		return ImmutableMap.copyOf(items);
+	}
+
+	/**
+	 * Returns all registered click events from the layout.
+	 * @return the underlying map
+	 */
+
+	public Map<Integer, Consumer<ClickEvent>> getClickEvents() {
+		return ImmutableMap.copyOf(clickEvents);
 	}
 }
