7e14b9b2c87ad37e41f31c4f9771f8c85d4fe192
theEvilReaper <steffenwx@gmail.com>
2021-01-17 17:13:31 +0100

    Added documentation

--- src/main/java/de/icevizion/aves/inventory/TranslatedCache.java
+++ src/main/java/de/icevizion/aves/inventory/TranslatedCache.java
@@ -7,7 +7,10 @@ import java.util.Map;
 import java.util.function.Function;
 
 /**
+ * The class acts as a cache for all inventories created with the system.
  * @author Nico (JumpingPxl) Middendorf
+ * @version 1.0.0
+ * @since 1.0.6
  */
 
 public class TranslatedCache {
@@ -18,9 +21,21 @@ public class TranslatedCache {
 		cachedInventories = Maps.newHashMap();
 	}
 
+	/**
+	 * Returns the map where all the inventories are stored.
+	 * @return the underlying map
+	 */
+
 	public Map<Locale, TranslatedInventory> getCachedInventories() {
 		return cachedInventories;
 	}
+
+	/**
+	 * Get an inventory from the underlying cache.
+	 * @param locale The language that acts as a key
+	 * @param function The function that includes the logic what happens when there is no inventory for the language
+	 * @return The fetched {@link TranslatedInventory}
+	 */
 
 	public TranslatedInventory getInventory(Locale locale,
 	                                        Function<Locale, TranslatedInventory> function) {
