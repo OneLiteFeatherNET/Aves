aa381fc299c4efd6735e5048b061dd63365f3eff
theEvilReaper <steffenwx@gmail.com>
2021-03-14 20:55:16 +0100

    Fix encoder

--- src/main/java/de/icevizion/aves/encoder/ItemStackEncoder.java
+++ src/main/java/de/icevizion/aves/encoder/ItemStackEncoder.java
@@ -1,40 +1,54 @@
 package de.icevizion.aves.encoder;
 
-import com.jsoniter.any.Any;
 import com.jsoniter.output.JsonStream;
 import com.jsoniter.spi.Encoder;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public final class ItemStackEncoder implements Encoder {
+
     @Override
-    public void encode(Object obj, JsonStream stream) throws IOException {
-        System.out.println(obj.toString());
-        if (obj instanceof ItemStack) {
-            ItemStack itemStack = (ItemStack) obj;
-            Any any = Any.wrapNull();
-            any.get("material").set(itemStack.getType().name());
-            any.get("amount").set(itemStack.getAmount());
-            if (itemStack.hasItemMeta()) {
-                Any meta = any.get("meta");
-                ItemMeta itemMeta = itemStack.getItemMeta();
-                if (itemMeta.hasDisplayName())
-                    meta.get("displayName").set(itemMeta.getDisplayName());
-                if (itemMeta.hasEnchants()) {
-                    for (Map.Entry<Enchantment, Integer> entry : itemMeta.getEnchants().entrySet()) {
-                        Any enchantmentObject = Any.wrapNull();
-                        enchantmentObject.get("enchantment").set(entry.getKey().getName());
-                        enchantmentObject.get("level").set(entry.getValue());
-                        meta.get("enchantments").asList().add(enchantmentObject);
+    public void encode(Object object, JsonStream jsonStream) throws IOException {
+        if (object == null) {
+            jsonStream.writeNull();
+            return;
+        } else {
+            if (object instanceof ItemStack) {
+                ItemStack stack = (ItemStack) object;
+                Map<String, Object> map = new HashMap<>();
+                map.put("material", stack.getType().name());
+                map.put("amount", stack.getAmount());
+                if (stack.hasItemMeta()) {
+                    var metaMap = new HashMap<String, Object>();
+                    var meta = stack.getItemMeta();
+                    if (meta.hasDisplayName())
+                        metaMap.put("displayName", meta.getDisplayName());
+                    if (!meta.getItemFlags().isEmpty())
+                        metaMap.put("flags", meta.getItemFlags().stream().map(itemFlag -> itemFlag.name()).collect(Collectors.toList()));
+
+                    if (!meta.getEnchants().isEmpty()) {
+                        var enchantments = new ArrayList<>();
+                        for (var entry : meta.getEnchants().entrySet()) {
+                            var enchantMap = new HashMap<String, Object>();
+                            enchantMap.put("enchantment", entry.getKey().getKey().getKey());
+                            enchantMap.put("level", entry.getValue());
+                            enchantments.add(enchantMap);
+                        }
+                        metaMap.put("enchantments", enchantments);
                     }
+                    if (meta.hasLore()) {
+                        metaMap.put("lore", meta.getLore());
+                    }
+
+                    map.put("meta", metaMap);
                 }
-                meta.bindTo(any);
-            }
-            any.writeTo(stream);
+                jsonStream.writeVal(HashMap.class, map);
+             }
         }
     }
 }
