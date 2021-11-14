7d2984061349b069af96c4e9d1d073dc528078d1
theEvilReaper <steffenwx@gmail.com>
2021-11-14 21:11:07 +0100

    Add annotation

--- src/main/java/de/icevizion/aves/inventory/util/ItemRegistry.java
+++ src/main/java/de/icevizion/aves/inventory/util/ItemRegistry.java
@@ -1,6 +1,7 @@
-package de.icevizion.aves.inventory;
+package de.icevizion.aves.inventory.util;
 
 import net.minestom.server.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.UUID;
@@ -13,7 +14,7 @@ public class ItemRegistry {
 
     private static final Map<ItemStack, UUID> registry = new WeakHashMap<>();
 
-    public static void register(ItemStack itemStack, ItemStack other) {
+    public static void register(@NotNull ItemStack itemStack, @NotNull ItemStack other) {
         var otherUuid = registry.get(other);
         if (otherUuid == null) {
             otherUuid = UUID.randomUUID();
@@ -22,7 +23,7 @@ public class ItemRegistry {
         registry.put(itemStack, otherUuid);
     }
 
-    public static boolean isSame(ItemStack itemStack, ItemStack other) {
+    public static boolean isSame(@NotNull ItemStack itemStack, @NotNull ItemStack other) {
         var uuid = registry.get(itemStack);
         if (uuid == null)
             return false;
