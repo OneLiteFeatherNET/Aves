8925a168a1193176069f278efb4c2c97b80fb928
theEvilReaper <steffenwx@gmail.com>
2022-01-22 20:35:34 +0100

    Add annotation

--- src/main/java/de/icevizion/aves/inventory/InventoryLayout.java
+++ src/main/java/de/icevizion/aves/inventory/InventoryLayout.java
@@ -6,6 +6,7 @@ import de.icevizion.aves.inventory.slot.TranslatedSlot;
 import net.minestom.server.event.inventory.InventoryPreClickEvent;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.ItemStackBuilder;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Locale;
@@ -13,6 +14,8 @@ import java.util.function.Consumer;
 
 /**
  * @author Patrick Zdarsky / Rxcki
+ * @since 1.0.12
+ * @version 1.0.0
  */
 
 public class InventoryLayout implements Cloneable {
@@ -23,7 +26,7 @@ public class InventoryLayout implements Cloneable {
 
     private ISlot[] contents;
 
-    public InventoryLayout(InventoryRows rows) {
+    public InventoryLayout(@NotNull InventoryRow rows) {
         this.contents = new ISlot[rows.getSize()];
     }
 
@@ -36,6 +39,7 @@ public class InventoryLayout implements Cloneable {
     }
 
     public void applyLayout(ItemStack[] itemStacks, Locale locale, MessageProvider messageProvider) {
+        if (itemStacks == null || itemStacks.length == 0) return;
         for (int i = 0; i < itemStacks.length; i++) {
             ISlot slot = contents[i];
 
@@ -179,7 +183,6 @@ public class InventoryLayout implements Cloneable {
      * Returns the array which contains all valid {@link ISlot}.
      * @return the underlying array
      */
-
     public ISlot[] getContents() {
         return contents;
     }
