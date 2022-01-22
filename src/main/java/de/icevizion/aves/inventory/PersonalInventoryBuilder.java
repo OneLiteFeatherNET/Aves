b9e5ec0fff6a8ba2853fcf41dd2f233a65f49cf4
theEvilReaper <steffenwx@gmail.com>
2022-01-22 20:31:58 +0100

    Add missing documentation

--- src/main/java/de/icevizion/aves/inventory/PersonalInventoryBuilder.java
+++ src/main/java/de/icevizion/aves/inventory/PersonalInventoryBuilder.java
@@ -3,22 +3,41 @@ package de.icevizion.aves.inventory;
 import net.minestom.server.entity.Player;
 
 /**
+ * The builder can be used to build a {@link net.minestom.server.inventory.Inventory} for a single {@link Player}.
  * @author Patrick Zdarsky / Rxcki
+ * @version 1.0.1
+ * @since 1.0.12
  */
 public class PersonalInventoryBuilder extends GlobalInventoryBuilder {
 
     private final Player player;
 
-    public PersonalInventoryBuilder(String title, InventoryRows rows, Player player) {
+    /**
+     * Creates a new instance from the {@link PersonalTranslatedInventoryBuilder} with the given values.
+     * @param title The title for the {@link net.minestom.server.inventory.Inventory}
+     * @param rows The amount of rows for the {@link net.minestom.server.inventory.Inventory}
+     * @param player The player who owns the {@link net.minestom.server.inventory.Inventory}
+     */
+    public PersonalInventoryBuilder(String title, InventoryRow rows, Player player) {
         super(title, rows);
         this.player = player;
     }
 
+    /**
+     * Creates a new instance from the {@link PersonalTranslatedInventoryBuilder} with the given values.
+     * @param title The title for the {@link net.minestom.server.inventory.Inventory}
+     * @param slots The amount of slots of the {@link net.minestom.server.inventory.Inventory}
+     * @param player The player who owns the {@link net.minestom.server.inventory.Inventory}
+     */
     public PersonalInventoryBuilder(String title, int slots, Player player) {
         super(title, slots);
         this.player = player;
     }
 
+    /**
+     * Opens the inventory for the given {@link Player}.
+     * The locale can not be null in this context because the class is a builder for translated inventories.
+     */
     public void openInventory() {
         player.openInventory(getInventory());
     }
