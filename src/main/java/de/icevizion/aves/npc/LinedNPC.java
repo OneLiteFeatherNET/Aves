d75625bfcc715601266fd7434c1f465359c4c2e7
theEvilReaper <steffenwx@gmail.com>
2022-02-11 00:27:20 +0100

    Improve hologramm generation

--- src/main/java/de/icevizion/aves/npc/LinedNPC.java
+++ src/main/java/de/icevizion/aves/npc/LinedNPC.java
@@ -1,12 +1,15 @@
 package de.icevizion.aves.npc;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
 import net.minestom.server.entity.PlayerSkin;
 import net.minestom.server.entity.hologram.Hologram;
 import net.minestom.server.instance.Instance;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -19,14 +22,28 @@ public class LinedNPC extends MinestomNPC {
 
     private final Hologram[] lines;
 
-    public LinedNPC(@NotNull UUID uuid, @NotNull Instance instance, @NotNull Component displayName, @NotNull PlayerSkin playerSkin, @NotNull Pos spawnPos, Component... lines) {
-        super(uuid, instance, displayName, playerSkin, spawnPos);
-        this.lines = new Hologram[lines.length];
+    public LinedNPC(@NotNull UUID uuid, @NotNull Instance instance, @NotNull String name, @NotNull Component displayName, @NotNull PlayerSkin playerSkin, @NotNull Pos spawnPos, List<String> lines) {
+        super(uuid, instance, name, displayName, playerSkin, spawnPos);
+        this.lines = new Hologram[lines.size()];
+
+        for (int i = 0; i < lines.size(); i++) {
+            var component = lines.get(i);
+            var hologram = new Hologram(instance, new Pos(spawnPos).add(0, 2 + (0.26 * i), 0), LegacyComponentSerializer.legacyAmpersand().deserialize(component));
+            this.lines[i] = hologram;
+        }
+    }
+
+    public void spawnLines() {
+        if (lines.length == 0) return;
 
         for (int i = 0; i < lines.length; i++) {
-            var component = lines[i];
-            var hologram = new Hologram(instance, new Pos(spawnPos).add(0, 1.7 + (0.3 * i), 0), component);
-            this.lines[i] = hologram;
+            lines[i].getEntity().spawn();
+        }
+    }
+
+    public void add(@NotNull Player player) {
+        for (int i = 0; i < lines.length; i++) {
+            lines[i].getEntity().addViewer(player);
         }
     }
 
