df58afc6f863a465c0753788228ea3231755df6f
theEvilReaper <steffenwx@gmail.com>
2022-03-18 18:51:23 +0100

    Improve position adapter to achieve that the instance works for vec and pos objects

--- src/main/java/de/icevizion/aves/file/gson/PositionGsonAdapter.java
+++ src/main/java/de/icevizion/aves/file/gson/PositionGsonAdapter.java
@@ -7,7 +7,9 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
 
 import java.lang.reflect.Type;
 
@@ -16,11 +18,10 @@ import java.lang.reflect.Type;
  * @version 1.0.0
  * @since 1.0.0
  **/
-
-public class PositionGsonAdapter implements JsonSerializer<Pos>, JsonDeserializer<Pos> {
+public class PositionGsonAdapter implements JsonSerializer<Point>, JsonDeserializer<Point> {
 
     @Override
-    public Pos deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+    public Point deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         JsonObject object = json.getAsJsonObject();
         double x = object.get("x").getAsDouble();
         double y = object.get("y").getAsDouble();
@@ -30,18 +31,23 @@ public class PositionGsonAdapter implements JsonSerializer<Pos>, JsonDeserialize
         if (object.has("yaw") && object.has("pitch")) {
             yaw = object.get("yaw").getAsFloat();
             pitch = object.get("pitch").getAsFloat();
+            return new Pos(x,y,z, yaw, pitch);
         }
-        return new Pos(x,y,z, yaw, pitch);
+        return new Vec(x,y,z);
     }
 
     @Override
-    public JsonElement serialize(Pos src, Type typeOfSrc, JsonSerializationContext context) {
+    public JsonElement serialize(Point src, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject object = new JsonObject();
         object.addProperty("x", src.x());
         object.addProperty("y", src.y());
         object.addProperty("z", src.z());
-        object.addProperty("yaw", src.yaw());
-        object.addProperty("pitch", src.pitch());
+
+        if (src instanceof Pos pos) {
+            object.addProperty("yaw", pos.yaw());
+            object.addProperty("pitch", pos.pitch());
+        }
+
         return object;
     }
 }
