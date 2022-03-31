b2bb8bb11c633a285a422c0c9e197b14ba55a9b4
theEvilReaper <steffenwx@gmail.com>
2022-03-31 18:09:59 +0200

    Add some documentation to the class

--- src/main/java/de/icevizion/aves/util/Positions.java
+++ src/main/java/de/icevizion/aves/util/Positions.java
@@ -3,10 +3,16 @@ package de.icevizion.aves.util;
 /**
  * @author theEvilReaper
  * @version 1.0.0
- * @since
+ * @since 1.0.0
  **/
 public class Positions {
 
+    /**
+     * Calculates the yaw value from two given double values.
+     * @param dX
+     * @param dZ
+     * @return the calculated yaw value
+     */
     public static float getYaw(double dX, double dZ) {
         return (float) Math.atan2(dZ, dX);
     }
