fba3a95397959730a78359207d5439c424689a45
theEvilReaper <steffenwx@gmail.com>
2022-04-29 21:33:40 +0200

    Swap argument usage

--- src/main/java/de/icevizion/aves/util/Positions.java
+++ src/main/java/de/icevizion/aves/util/Positions.java
@@ -9,11 +9,11 @@ public class Positions {
 
     /**
      * Calculates the yaw value from two given double values.
-     * @param dX
-     * @param dZ
+     * @param dX The x value
+     * @param dZ The y value
      * @return the calculated yaw value
      */
     public static float getYaw(double dX, double dZ) {
-        return (float) Math.atan2(dZ, dX);
+        return (float) Math.atan2(dX, dZ);
     }
 }
