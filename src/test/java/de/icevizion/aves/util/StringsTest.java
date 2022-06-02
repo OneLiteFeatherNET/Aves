6a3fab4b3c0bc11de173b9a4b52f1fadcb98c833
theEvilReaper <steffenwx@gmail.com>
2022-06-02 19:23:08 +0200

    Add new test case and rename existing test case

--- src/test/java/de/icevizion/aves/util/StringsTest.java
+++ src/test/java/de/icevizion/aves/util/StringsTest.java
@@ -7,8 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringsTest {
 
     @Test
-    void getTimeString() {
+    void testZeroTime() {
         var expectedFormat = "00:00";
         assertEquals(expectedFormat, Strings.getTimeString(0));
+    }
+    @Test
+    void testToHighFormat() {
+        var excepted = "70:00";
+        assertEquals(excepted, Strings.getTimeString(4200));
     }
 }
