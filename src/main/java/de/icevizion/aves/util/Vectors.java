58511b7f967f9e2176d080a59d8661d91976dbc1
theEvilReaper <steffenwx@gmail.com>
2022-07-07 12:55:46 +0200

    Add some annotations and class documentation header

--- src/main/java/de/icevizion/aves/util/Vectors.java
+++ src/main/java/de/icevizion/aves/util/Vectors.java
@@ -2,14 +2,22 @@ package de.icevizion.aves.util;
 
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.coordinate.Vec;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
 
+/**
+ * The class contains some usefully method to work with vectors.
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public class Vectors {
 
     private final static Random random = new Random();
 
+    @NotNull
     public static Vec getRandomVector() {
         double x = random.nextDouble() * 2.0D - 1.0D;
         double y = random.nextDouble() * 2.0D - 1.0D;
@@ -17,12 +25,15 @@ public class Vectors {
         return new Vec(x, y, z).normalize();
     }
 
+    @Contract("_ -> new")
+    @NotNull
     public static Vec getBackVector(@NotNull Pos location) {
         float newZ = (float)(location.z() + Math.sin(Math.toRadians(location.yaw() + 90.0F)));
         float newX = (float)(location.x() + Math.cos(Math.toRadians(location.yaw() + 90.0F)));
         return new Vec(newX - location.x(), 0.0D, newZ - location.z());
     }
 
+    @NotNull
     public static Vec getRandomCircleVector() {
         double rnd = random.nextDouble() * 2.0D * Math.PI;
         double x = Math.cos(rnd);
