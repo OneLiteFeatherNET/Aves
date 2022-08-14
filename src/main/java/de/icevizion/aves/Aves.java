77717923fda13b2a4fdb78d3a8a7d55d3999aa53
theEvilReaper <steffenwx@gmail.com>
2022-08-14 17:08:28 +0200

    Remove jsoniter integration

--- src/main/java/de/icevizion/aves/Aves.java
+++ src/main/java/de/icevizion/aves/Aves.java
@@ -1,11 +1,6 @@
 package de.icevizion.aves;
 
-import com.jsoniter.spi.JsoniterSpi;
-import com.jsoniter.spi.TypeLiteral;
-import de.icevizion.aves.file.jsoniter.ItemStackDecoder;
-import de.icevizion.aves.file.jsoniter.ItemStackEncoder;
 import net.minestom.server.extensions.Extension;
-import net.minestom.server.item.ItemStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,9 +18,6 @@ public final class Aves extends Extension {
                  / ____ \\ V /  __/\\__ \\
                 /_/    \\_\\_/ \\___||___/""");
         AVES_LOGGER.info("Starting Aves....");
-        TypeLiteral.nativeTypes.put(ItemStack.class, TypeLiteral.NativeType.OBJECT);
-        JsoniterSpi.registerTypeDecoder(ItemStack.class, new ItemStackDecoder());
-        JsoniterSpi.registerTypeEncoder(ItemStack.class, new ItemStackEncoder());
     }
 
     @Override
