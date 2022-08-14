2220e35bdb0f06c7c960ea9d1c5fcdd7549c7ac7
theEvilReaper <steffenwx@gmail.com>
2022-08-14 16:22:21 +0200

    Change printlns to a logger call

--- src/main/java/de/icevizion/aves/Aves.java
+++ src/main/java/de/icevizion/aves/Aves.java
@@ -6,19 +6,23 @@ import de.icevizion.aves.file.jsoniter.ItemStackDecoder;
 import de.icevizion.aves.file.jsoniter.ItemStackEncoder;
 import net.minestom.server.extensions.Extension;
 import net.minestom.server.item.ItemStack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class Aves extends Extension {
 
+    private static final Logger AVES_LOGGER = LoggerFactory.getLogger(Aves.class);
+
     @Override
     public void initialize() {
-        System.out.println("""
+        AVES_LOGGER.info("""
 
                     /\\                \s
                    /  \\__   _____  ___\s
                   / /\\ \\ \\ / / _ \\/ __|
                  / ____ \\ V /  __/\\__ \\
                 /_/    \\_\\_/ \\___||___/""");
-        System.out.println("Starting Aves....");
+        AVES_LOGGER.info("Starting Aves....");
         TypeLiteral.nativeTypes.put(ItemStack.class, TypeLiteral.NativeType.OBJECT);
         JsoniterSpi.registerTypeDecoder(ItemStack.class, new ItemStackDecoder());
         JsoniterSpi.registerTypeEncoder(ItemStack.class, new ItemStackEncoder());
@@ -26,6 +30,6 @@ public final class Aves extends Extension {
 
     @Override
     public void terminate() {
-
+        // Nothing to do
     }
 }
