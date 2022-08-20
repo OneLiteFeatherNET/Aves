f7231693d13c12d7b4b29eb09ca65abb504c020b
theEvilReaper <steffenwx@gmail.com>
2022-08-20 10:02:00 +0200

    Add dependencies to mock classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
 
     testImplementation("com.github.Minestom:Minestom:$minestomVersion")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testImplementation("org.mockito:mockito-core:4.6.1");
+    testImplementation("org.mockito:mockito-junit-jupiter:4.6.1");
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
