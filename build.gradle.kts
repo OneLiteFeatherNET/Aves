9601ce58f0a2f010fafdfce3a248d87148f0e665
theEvilReaper <steffenwx@gmail.com>
2022-08-21 00:07:04 +0200

    Add jacoco plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.sonarqube.gradle.SonarQubeTask
+
 plugins {
     java
     `java-library`
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.sonarqube") version "3.4.0.2513"
+    jacoco
 }
 
 group = "de.icevizion.lib"
@@ -46,11 +49,23 @@ tasks {
         dependsOn("shadowJar")
     }
 
+    jacocoTestReport {
+        dependsOn(rootProject.tasks.test)
+        reports {
+            xml.required.set(true)
+        }
+    }
+
     test {
+        finalizedBy(rootProject.tasks.jacocoTestReport)
         useJUnitPlatform()
         testLogging {
             events("passed", "skipped", "failed")
         }
+    }
+
+    getByName<SonarQubeTask>("sonarqube") {
+        dependsOn(rootProject.tasks.test)
     }
 }
 
