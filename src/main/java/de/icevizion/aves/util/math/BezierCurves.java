8a7043b6ed20c5e203a97a415092fb56239e08e0
theEvilReaper <steffenwx@gmail.com>
2022-08-20 17:27:09 +0200

    Remove test method and add NotNull,Contract annotation

--- src/main/java/de/icevizion/aves/util/math/BezierCurves.java
+++ src/main/java/de/icevizion/aves/util/math/BezierCurves.java
@@ -1,10 +1,9 @@
 package de.icevizion.aves.util.math;
 
-import de.icevizion.aves.util.Particles;
 import de.icevizion.aves.util.vector.Vec2D;
 import net.minestom.server.coordinate.Vec;
-import net.minestom.server.entity.Player;
-import net.minestom.server.particle.Particle;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author theEvilReaper
@@ -15,7 +14,8 @@ public final class BezierCurves {
 
     private BezierCurves() {}
 
-    public static Vec2D quadraticBezier(Vec start, Vec control, Vec end, float delta) {
+    @Contract("_, _, _, _ -> new")
+    public static @NotNull Vec2D quadraticBezier(@NotNull Vec start, @NotNull Vec control, @NotNull Vec end, float delta) {
         var xFinal = Math.pow(1 - delta, 2) * start.x() + (1- delta) * 2 * delta * control.x() +
                 delta * delta * end.x();
 
@@ -25,17 +25,7 @@ public final class BezierCurves {
         return new Vec2D(xFinal, zFinal);
     }
 
-    public static void test(Player player, Vec start, Vec control, Vec end) {
-        for (float t = 0; t <= 1; t+= 0.01) {
-            var xFinal = quadraticBezier(start.x(), control.x(), end.x(), t);
-            var yFinal = quadraticBezier(start.y(), control.y(), end.y(), t);
-            var zFinal  = quadraticBezier(start.z(), control.z(), end.z(), t);
-            Particles.playPoint(Particle.WITCH, player, new Vec(xFinal, yFinal, zFinal));
-        }
-    }
-
     public static double quadraticBezier(double p1, double p2, double p3, float t) {
         return ( 1.0 - t ) * (( 1.0 - t ) * p1 + t * p2) + t * (( 1.0 - t ) * p2 + t * p3);
     }
-
 }
