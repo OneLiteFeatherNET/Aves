d2dfa3a94b9c8ab1dd0c9e0d3686d9bbc39e8710
theEvilReaper <steffenwx@gmail.com>
2022-08-21 11:08:29 +0200

    Add some new tests

--- src/test/java/de/icevizion/aves/inventory/LayoutCalculatorsTest.java
+++ src/test/java/de/icevizion/aves/inventory/LayoutCalculatorsTest.java
@@ -1,11 +1,10 @@
 package de.icevizion.aves.inventory;
 
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 import de.icevizion.aves.inventory.util.LayoutCalculator;
 import net.minestom.server.inventory.InventoryType;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Patrick Zdarsky / Rxcki
@@ -17,6 +16,13 @@ class LayoutCalculatorsTest {
         var arr = LayoutCalculator.repeat(0, 3);
         var expected = new int[]{0,1,2};
         assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    void failRepeat() {
+        assertThrowsExactly(IllegalArgumentException.class,
+                () -> LayoutCalculator.repeat(0, -1),
+                "fromSlot cannot be higher that toSlot!");
     }
 
     @Test
@@ -36,6 +42,19 @@ class LayoutCalculatorsTest {
     }
 
     @Test
+    void frame() {
+        var arr = LayoutCalculator.frame(10, 31);
+        var expected = new int[] {10, 28, 11, 29, 12, 30, 13, 31, 19, 22};
+
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    void failFrame() {
+        assertThrowsExactly(IllegalArgumentException.class, () -> LayoutCalculator.frame(10, 10), "The values are the same");
+    }
+
+    @Test
     void fillColumn() {
         var arr = LayoutCalculator.fillColumn(InventoryType.CHEST_4_ROW, 2);
         var expected = new int[]{2, 11, 20, 29};
@@ -44,10 +63,18 @@ class LayoutCalculatorsTest {
     }
 
     @Test
-    void frame() {
-        var arr = LayoutCalculator.frame(10, 31);
-        var expected = new int[] {10, 28, 11, 29, 12, 30, 13, 31, 19, 22};
+    void failFillColumnOnColumnParamToSmall() {
+        assertThrowsExactly(IllegalArgumentException.class, () -> LayoutCalculator.fillColumn(InventoryType.CHEST_5_ROW, -1), "Column cant be less than 0 or more than 8");
+    }
 
-        assertArrayEquals(expected, arr);
+    @Test
+    void failFillColumnOnColumnParamToHigh() {
+        assertThrowsExactly(IllegalArgumentException.class, () -> LayoutCalculator.fillColumn(InventoryType.CHEST_5_ROW, 10), "Column cant be less than 0 or more than 8");
+    }
+
+    @Test
+    void testGetRowCount() {
+        assertSame(1, LayoutCalculator.getRowCount(InventoryType.BEACON));
+        assertSame(1, LayoutCalculator.getRowCount(InventoryType.CARTOGRAPHY));
     }
 }
