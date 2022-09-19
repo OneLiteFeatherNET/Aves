1aa719a0226a0fc5213e84931b13a725eaefed3f
theEvilReaper <steffenwx@gmail.com>
2022-09-19 20:07:11 +0200

    Add own class for inventory constants

---
+++ src/main/java/de/icevizion/aves/inventory/util/InventoryConstants.java
@@ -0,0 +1,27 @@
+package de.icevizion.aves.inventory.util;
+
+import de.icevizion.aves.inventory.InventorySlot;
+import de.icevizion.aves.inventory.function.InventoryClick;
+import net.minestom.server.event.trait.CancellableEvent;
+import net.minestom.server.item.ItemStack;
+
+import java.util.function.Consumer;
+
+/**
+ * Contains some constant which are used for inventories
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+
+public class InventoryConstants {
+
+    public static final InventoryClick CANCEL_CLICK = (player, clickType, slotID, condition) -> condition.setCancel(true);
+
+    public static final Consumer<CancellableEvent> CANCELLABLE_EVENT = event -> event.setCancelled(true);
+
+    public static final InventorySlot EMPTY_SLOT = new InventorySlot(ItemStack.AIR);
+
+    private InventoryConstants() {}
+
+}
