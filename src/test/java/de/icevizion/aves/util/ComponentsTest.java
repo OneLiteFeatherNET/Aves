3b2ca4254e89119e9710f392f4f2a7db03a667a9
theEvilReaper <steffenwx@gmail.com>
2022-10-07 19:49:17 +0200

    Add test for the components class

---
+++ src/test/java/de/icevizion/aves/util/ComponentsTest.java
@@ -0,0 +1,33 @@
+package de.icevizion.aves.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+
+import static de.icevizion.aves.util.Components.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComponentsTest {
+
+    private final Component HEART_STRING = getHealthString(10);
+
+    @Test
+    void testGetHealthString() {
+        var component = getHealthString(20D);
+        assertNotEquals(convertToString(HEART_STRING), convertToString(component));
+    }
+
+    @Test
+    void testGetHealthStringWithVariables() {
+        var component = getHealthString(10, NamedTextColor.AQUA, NamedTextColor.BLUE);
+        assertNotEquals(convertToString(HEART_STRING), convertToString(component));
+    }
+
+    private String convertToString(@NotNull Component component) {
+        return LegacyComponentSerializer.legacyAmpersand().serialize(HEART_STRING);
+    }
+
+
+}
