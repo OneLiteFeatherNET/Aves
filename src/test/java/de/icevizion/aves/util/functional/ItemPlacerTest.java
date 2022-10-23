ff9e7b31c93e04a6b04f5e13280c0aeddd930f78
theEvilReaper <steffenwx@gmail.com>
2022-10-23 20:01:38 +0200

    Add test for the ItemPlacer interface

---
+++ src/test/java/de/icevizion/aves/util/functional/ItemPlacerTest.java
@@ -0,0 +1,41 @@
+package de.icevizion.aves.util.functional;
+
+import de.icevizion.aves.item.Item;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
+import net.minestom.testing.Env;
+import net.minestom.testing.EnvTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@EnvTest
+class ItemPlacerTest {
+
+    @Test
+    void testDefaultPlacer(Env env) {
+        var instance = env.createFlatInstance();
+        var player = env.createPlayer(instance, Pos.ZERO);
+        ItemPlacer.FALLBACK.setItem(player, 0, Item.of(Material.SADDLE), null);
+        env.destroyInstance(instance);
+    }
+
+    @Test
+    void testSetArmorSet(Env env) {
+        var instance = env.createFlatInstance();
+        var player = env.createPlayer(instance, Pos.ZERO);
+        var helmetItem = Item.of(ItemStack.builder(Material.LEATHER_HELMET));
+        var chestPlateItem = Item.of(ItemStack.builder(Material.LEATHER_CHESTPLATE));
+        var leggingsItem = Item.of(ItemStack.builder(Material.LEATHER_LEGGINGS));
+        ItemPlacer.FALLBACK.setItem(player, 0, helmetItem, null, true);
+        ItemPlacer.FALLBACK.setItem(player, 1, chestPlateItem, null, true);
+        ItemPlacer.FALLBACK.setItem(player, 2, leggingsItem, null, true);
+        ItemPlacer.FALLBACK.setItem(player, 3, leggingsItem, null, true);
+
+        Assertions.assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> ItemPlacer.FALLBACK.setItem(player, -1, leggingsItem, null, true),
+                "The slotID is greater than four"
+        );
+    }
+}
