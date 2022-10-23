b87085d90f0f77ae90090d239aff1c25f8e0ec8f
theEvilReaper <steffenwx@gmail.com>
2022-10-23 16:32:44 +0200

    Outsource Env tests into own class

--- src/test/java/de/icevizion/aves/util/BroadcasterTest.java
+++ src/test/java/de/icevizion/aves/util/BroadcasterTest.java
@@ -3,33 +3,22 @@ package de.icevizion.aves.util;
 import net.kyori.adventure.text.Component;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
-import net.minestom.testing.Env;
-import net.minestom.testing.EnvTest;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@EnvTest
 class BroadcasterTest {
 
     static Component text = Component.text("Test");
 
     @Test
     void testBroadcastWithEmptyList() {
-        List<Player> players = Collections.emptyList();
+        List<Player> players = new ArrayList<>();
         Broadcaster.broadcast(players, text);
         assertNotNull(text);
     }
 
-    @Test
-    void testBroadcast(Env env) {
-        var instance = env.createFlatInstance();
-        env.createPlayer(instance, Pos.ZERO);
-
-        Broadcaster.broadcast(instance, text);
-        assertNotNull(instance);
-    }
 }
