b37ebf5ea4091bf6592829ad7825f3d71aaa8688
theEvilReaper <steffenwx@gmail.com>
2022-10-23 20:01:17 +0200

    Fix broadcast test which send a message to a collection of players

--- src/test/java/de/icevizion/aves/util/BroadcasterEnvTest.java
+++ src/test/java/de/icevizion/aves/util/BroadcasterEnvTest.java
@@ -6,6 +6,8 @@ import net.minestom.testing.Env;
 import net.minestom.testing.EnvTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @EnvTest
@@ -21,12 +23,20 @@ class BroadcasterEnvTest {
 
         Broadcaster.broadcast(instance, text);
         assertNotNull(instance);
+        env.destroyInstance(instance);
     }
 
     @Test
     void testBroadcastToAll(Env env) {
-        var connection = env.createConnection();
-        Broadcaster.broadcast(text);
-        assertNotNull(connection);
+        var instance = env.createFlatInstance();
+        var playerOne = env.createPlayer(instance, Pos.ZERO);
+        var playerTwo = env.createPlayer(instance, Pos.ZERO);
+
+        assertNotNull(playerOne);
+        assertNotNull(playerTwo);
+
+        Broadcaster.broadcast(List.of(playerOne, playerTwo), Component.text("Hallo"));
+
+        env.destroyInstance(instance);
     }
 }
