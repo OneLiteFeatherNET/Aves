6ccb565ca1e67717db2f66cb2faa0b8041a58aa1
Phillipp Glanz <p.glanz@madfix.me>
2022-10-28 06:27:57 +0000

    Update plugin org.sonarqube to v3.5.0.2730

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     `java-library`
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("org.sonarqube") version "3.4.0.2513"
+    id("org.sonarqube") version "3.5.0.2730"
     jacoco
 }
 
