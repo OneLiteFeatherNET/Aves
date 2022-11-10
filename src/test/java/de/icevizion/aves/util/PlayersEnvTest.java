95f3f8890a595d29719550862a80d55e537bfae4
theEvilReaper <steffenwx@gmail.com>
2022-11-10 22:56:18 +0100

    Add an item to the inventory

--- src/test/java/de/icevizion/aves/util/PlayersEnvTest.java
+++ src/test/java/de/icevizion/aves/util/PlayersEnvTest.java
@@ -2,6 +2,8 @@ package de.icevizion.aves.util;
 
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.instance.Instance;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
 import net.minestom.testing.Env;
 import net.minestom.testing.EnvTest;
 import org.jetbrains.annotations.NotNull;
@@ -32,6 +34,7 @@ class PlayersEnvTest {
     @Test
     void testDropInventory(@NotNull Env env) {
         var player = env.createPlayer(instance, Pos.ZERO);
+        player.getInventory().addItemStack(ItemStack.builder(Material.STONE).build());
         Players.dropPlayerInventory(player);
         assertNotNull(player);
     }
