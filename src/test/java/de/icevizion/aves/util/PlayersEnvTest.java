d00e0e4723bc1f70ea2971b40c299843828f825a
theEvilReaper <steffenwx@gmail.com>
2022-11-10 22:47:38 +0100

    Add new test

--- src/test/java/de/icevizion/aves/util/PlayersEnvTest.java
+++ src/test/java/de/icevizion/aves/util/PlayersEnvTest.java
@@ -4,6 +4,7 @@ import net.minestom.server.coordinate.Pos;
 import net.minestom.server.instance.Instance;
 import net.minestom.testing.Env;
 import net.minestom.testing.EnvTest;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,7 @@ class PlayersEnvTest {
     static Instance instance;
 
     @BeforeAll
-    static void init(Env env) {
+    static void init(@NotNull Env env) {
         instance = env.createFlatInstance();
         for (int i = 0; i < 3; i++) {
             env.createPlayer(instance, Pos.ZERO);
@@ -26,5 +27,11 @@ class PlayersEnvTest {
     void testRandomPlayer(Env env) {
         var randomPlayer = Players.getRandomPlayer();
         assertFalse(randomPlayer::isPresent);
+    }
+
+    @Test
+    void testDropInventory(@NotNull Env env) {
+        var player = env.createPlayer(instance, Pos.ZERO);
+        Players.dropPlayerInventory(player);
     }
 }
