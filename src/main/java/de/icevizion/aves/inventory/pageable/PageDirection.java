ab242f773432e83a6bd39b6188160c5721bfbdc5
theEvilReaper <steffenwx@gmail.com>
2022-12-31 15:03:26 +0100

    Add PageDirection enum

---
+++ src/main/java/de/icevizion/aves/inventory/pageable/PageDirection.java
@@ -0,0 +1,13 @@
+package de.icevizion.aves.inventory.pageable;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.2.0
+ */
+public enum PageDirection {
+
+    UPDATE,
+    FORWARD,
+    BACKWARDS
+}
