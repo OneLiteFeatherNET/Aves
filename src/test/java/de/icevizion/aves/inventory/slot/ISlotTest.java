e05f409db3d00110bfad6d3477999d5043c3874c
theEvilReaper <steffenwx@gmail.com>
2023-01-07 14:26:41 +0100

    Add test for the new copy method

--- src/test/java/de/icevizion/aves/inventory/slot/ISlotTest.java
+++ src/test/java/de/icevizion/aves/inventory/slot/ISlotTest.java
@@ -2,6 +2,7 @@ package de.icevizion.aves.inventory.slot;
 
 import de.icevizion.aves.inventory.InventorySlot;
 import de.icevizion.aves.inventory.util.InventoryConstants;
+import de.icevizion.aves.item.TranslatedItem;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import org.junit.jupiter.api.Test;
@@ -51,5 +52,17 @@ class ISlotTest {
         var slot = new InventorySlot(ItemStack.builder(Material.ACACIA_BOAT).build());
         System.out.println(slot);
         assertNotSame("empty", slot.toString());
+    }
+
+    @Test
+    void testCopyMethod() {
+        var inventorySlot = new InventorySlot(ItemStack.builder(Material.ACACIA_BOAT));
+        var copiedSLot = ISlot.of(inventorySlot);
+        assertEquals(inventorySlot, copiedSLot);
+
+        var translatedSlot = new TranslatedSlot(TranslatedItem.of(Material.ALLIUM));
+        copiedSLot = ISlot.of(translatedSlot);
+        //assertEquals(translatedSlot.itemStack, copiedSLot.getItem());
+        assertSame(translatedSlot.inventoryClick, copiedSLot.getClick());
     }
 }
