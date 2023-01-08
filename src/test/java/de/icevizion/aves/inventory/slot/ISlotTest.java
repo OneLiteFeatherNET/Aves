c6703df9c3c85373153381e81fa4d7bc7418efea
theEvilReaper <steffenwx@gmail.com>
2023-01-08 13:31:23 +0100

    Update slot tests

--- src/test/java/de/icevizion/aves/inventory/slot/ISlotTest.java
+++ src/test/java/de/icevizion/aves/inventory/slot/ISlotTest.java
@@ -2,7 +2,6 @@ package de.icevizion.aves.inventory.slot;
 
 import de.icevizion.aves.inventory.InventorySlot;
 import de.icevizion.aves.inventory.util.InventoryConstants;
-import de.icevizion.aves.item.TranslatedItem;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import org.junit.jupiter.api.Test;
@@ -12,57 +11,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class ISlotTest {
 
     @Test
-    void testSlotCreation() {
-        var slot = new InventorySlot(ItemStack.AIR);
-        assertSame(ItemStack.AIR, slot.getItem());
+    void testGetter() {
+        ISlot iSlot = new InventorySlot(ItemStack.AIR, InventoryConstants.CANCEL_CLICK);
+        assertSame(Material.AIR, iSlot.getItem().material());
+        assertEquals(InventoryConstants.CANCEL_CLICK, iSlot.getClick());
     }
 
     @Test
-    void testOtherSlotCreation() {
-        var slot = new InventorySlot(ItemStack.AIR, InventoryConstants.CANCEL_CLICK);
-        assertSame(ItemStack.AIR, slot.getItem());
-        assertSame(InventoryConstants.CANCEL_CLICK, slot.getClick());
+    void testSetter() {
+        ISlot iSlot = new InventorySlot(ItemStack.AIR);
+        assertSame(Material.AIR, iSlot.getItem().material());
+        assertNull(iSlot.getClick());
+        iSlot.setItemStack(ItemStack.of(Material.ACACIA_FENCE));
+        assertNotSame(Material.AIR, iSlot.getItem().material());
+        iSlot.setClick(InventoryConstants.CANCEL_CLICK);
+        assertNotNull(iSlot.getClick());
     }
 
     @Test
-    void testCreationWithBuilder() {
-        var slot = new InventorySlot(ItemStack.builder(Material.ACACIA_BOAT).amount(2));
-        assertSame(Material.ACACIA_BOAT, slot.getItem().material());
-        assertNotEquals(12, slot.getItem().amount());
-    }
+    void testCopyWithNullReturn() {
+        ISlot iSlot = new UnknownSlotImpl();
+        ISlot copiedSlot = ISlot.of(iSlot);
 
-    @Test
-    void testCopySlotCreation() {
-        var slot = new InventorySlot(ItemStack.AIR, InventoryConstants.CANCEL_CLICK);
-        var copySlot = InventorySlot.of(slot);
-        assertSame(slot.getItem(), copySlot.getItem());
-        assertSame(slot.getClick(), copySlot.getClick());
-        assertNotSame(slot, copySlot);
-    }
-
-    @Test
-    void setItemSet() {
-        var slot = new InventorySlot(ItemStack.AIR, InventoryConstants.CANCEL_CLICK);
-        slot.setItemStack(ItemStack.builder(Material.ALLIUM).build());
-        assertNotSame(ItemStack.AIR, slot.getItem());
-    }
-
-    @Test
-    void testToString() {
-        var slot = new InventorySlot(ItemStack.builder(Material.ACACIA_BOAT).build());
-        System.out.println(slot);
-        assertNotSame("empty", slot.toString());
-    }
-
-    @Test
-    void testCopyMethod() {
-        var inventorySlot = new InventorySlot(ItemStack.builder(Material.ACACIA_BOAT));
-        var copiedSLot = ISlot.of(inventorySlot);
-        assertEquals(inventorySlot, copiedSLot);
-
-        var translatedSlot = new TranslatedSlot(TranslatedItem.of(Material.ALLIUM));
-        copiedSLot = ISlot.of(translatedSlot);
-        //assertEquals(translatedSlot.itemStack, copiedSLot.getItem());
-        assertSame(translatedSlot.inventoryClick, copiedSLot.getClick());
+        assertNotNull(iSlot);
+        assertNull(copiedSlot);
     }
 }
