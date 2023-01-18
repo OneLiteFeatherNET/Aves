c58ea4590423dd7ece9f9bc2e00aab64b7393307
theEvilReaper <steffenwx@gmail.com>
2023-01-18 16:25:48 +0100

    Add missing documenation

--- src/main/java/de/icevizion/aves/inventory/GlobalInventoryBuilder.java
+++ src/main/java/de/icevizion/aves/inventory/GlobalInventoryBuilder.java
@@ -11,6 +11,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Locale;
 
 /**
+ * The {@link GlobalInventoryBuilder} builds an inventory which can be used in a global context.
+ * That means that the inventory is related to all player's on the server and not bound to a single player.
  * @author Patrick Zdarsky / Rxcki
  * @version 1.0.0
  * @since 1.0.0
@@ -21,17 +23,31 @@ public class GlobalInventoryBuilder extends BaseInventoryBuilderImpl {
     private Component titleComponent;
     private CustomInventory inventory;
 
+    /**
+     * Creates a new instance from the builder with the given parameter values.
+     * @param title the title as {@link Component }for the inventory
+     * @param type the {@link InventoryType} for the inventory
+     */
     public GlobalInventoryBuilder(@NotNull Component title, @NotNull InventoryType type) {
         super(type);
         this.titleComponent = title;
         this.holder = new InventoryHolderImpl(this);
     }
 
+    /**
+     * Updates the title for the inventory
+     * @param titleComponent the new title as {@link Component} to set
+     */
     public void setTitleComponent(@NotNull Component titleComponent) {
         this.titleComponent = titleComponent;
         this.updateTitle(inventory, titleComponent);
     }
 
+    /**
+     * Returns the inventory with the current state of the items.
+     * @param ignored can be ignored
+     * @return the underlying inventory
+     */
     @Override
     public Inventory getInventory(Locale ignored) {
         if (dataLayoutValid && inventoryLayoutValid && inventory != null)
@@ -49,6 +65,9 @@ public class GlobalInventoryBuilder extends BaseInventoryBuilderImpl {
         return inventory != null && !inventory.getViewers().isEmpty();
     }
 
+    /**
+     * Updates the inventory content if something received a change.
+     */
     @Override
     protected void updateInventory() {
         boolean applyLayout = !inventoryLayoutValid;
@@ -61,6 +80,9 @@ public class GlobalInventoryBuilder extends BaseInventoryBuilderImpl {
         updateViewer(inventory);
     }
 
+    /**
+     * Applies the data layout to the inventory, if the specific layout is set.
+     */
     @Override
     protected void applyDataLayout() {
         synchronized (this) {
