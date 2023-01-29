d5961e0ff2dce92090b7bc773fcea13f0bfef41f
Phillipp Glanz <p.glanz@madfix.me>
2023-01-29 17:21:48 +0100

    With cache node

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,20 @@
 rootProject.name = "Aves"
+val ci = System.getenv().containsKey("CI")
+
+buildCache {
+    local {
+        isEnabled = !ci
+    }
+    if (ci) {
+        remote(HttpBuildCache::class) {
+            url = uri(System.getenv("GRADLE_CACHE_URL"))
+            isPush = ci
+            isAllowUntrustedServer = true
+            isAllowInsecureProtocol = true
+            credentials {
+                username = System.getenv("GRADLE_CACHE_USER")
+                password = System.getenv("GRADLE_CACHE_PASSWORD")
+            }
+        }
+    }
+}
