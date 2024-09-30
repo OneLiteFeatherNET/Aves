6589225f1f153c829cc572ae33c69fff84de0893
theEvilReaper <steffenwx@gmail.com>
2024-09-30 21:53:11 +0200

    Improve documentation

--- src/main/java/de/icevizion/aves/util/functional/PlayerConsumer.java
+++ src/main/java/de/icevizion/aves/util/functional/PlayerConsumer.java
@@ -28,8 +28,7 @@ public interface PlayerConsumer {
     /**
      * Returns a composed {@code PlayerConsumer} that performs, in sequence, this
      * operation followed by the {@code after} operation.
-     * If performing either operation throws an exception, it is relayed to the caller of the
-     * composed operation.
+     * If performing either operation throws an exception, it is relayed to the caller of the composed operation.
      * If performing this operation throws an exception, the {@code after} operation will not be performed.
      *
      * @param after the operation to perform after this operation
