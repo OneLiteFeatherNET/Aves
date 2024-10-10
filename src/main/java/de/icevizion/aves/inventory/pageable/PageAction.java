8a326f89887097d8b6c119b4594a23e8db630995
theEvilReaper <steffenwx@gmail.com>
2024-10-10 11:54:25 +0200

    Deprecate UPDATE enum entry and add REFRESH as replacement

--- src/main/java/de/icevizion/aves/inventory/pageable/PageAction.java
+++ src/main/java/de/icevizion/aves/inventory/pageable/PageAction.java
@@ -1,7 +1,7 @@
 package de.icevizion.aves.inventory.pageable;
 
 /**
- * The enum contains all actions which can be applied to a inventory page.
+ * The enum contains all actions which can be applied to an inventory page.
  * With {@link PageAction#FORWARD} or {@link PageAction#BACKWARDS} trigger a page switch.
  * The {@link PageAction#UPDATE} is used to update the content from a page
  * @author theEvilReaper
@@ -10,7 +10,9 @@ package de.icevizion.aves.inventory.pageable;
  */
 public enum PageAction {
 
+    @Deprecated(forRemoval = true, since = "Please use REFRESH instead")
     UPDATE,
+    REFRESH,
     FORWARD,
     BACKWARDS
 }
