91ffaf3ee6d027040279a66e90db4de0c864f810
Steffen Wonning <6805083+theEvilReaper@users.noreply.github.com>
2025-10-11 22:46:34 +0200

    Remove publishdata plugin usage (#45)
    * Remove publishdata plugin definition and plugin portal integration
    
    * Switch from publishdata to the standard publishing style
    
    * Revert downgrade of the mycelium bom version
    
    ---------
    
    Co-authored-by: theEvilReaper <theEvilReaper@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,5 @@
 rootProject.name = "aves"
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://eldonexus.de/repository/maven-public/")
-    }
-}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
@@ -27,7 +22,6 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("bom", "1.4.5")
-            version("publishdata", "1.4.0")
             library("mycelium.bom", "net.onelitefeather", "mycelium-bom").versionRef("bom")
             library("minestom","net.minestom", "minestom").withoutVersion()
             library("adventure", "net.kyori", "adventure-text-minimessage").withoutVersion()
@@ -35,8 +29,6 @@ dependencyResolutionManagement {
             library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").withoutVersion()
             library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").withoutVersion()
             library("junit.platform.launcher", "org.junit.platform", "junit-platform-launcher").withoutVersion()
-
-            plugin("publishdata", "de.chojo.publishdata").versionRef("publishdata")
         }
     }
 }
