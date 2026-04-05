21e085b41811349d255b93a37c8ddafa1a068b01
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-04-05 16:01:57 +0200

    chore(inventory): add package info files and improve annotation handling

--- src/main/java/net/theevilreaper/aves/inventory/holder/InventoryHolder.java
+++ src/main/java/net/theevilreaper/aves/inventory/holder/InventoryHolder.java
@@ -1,19 +1,20 @@
 package net.theevilreaper.aves.inventory.holder;
 
 import net.minestom.server.inventory.Inventory;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a wrapper objects which holds an instance from an inventory
+ *
  * @author theEvilReaper
- * @version 1.0.0
+ * @version 1.0.1
  * @since 1.2.0
  */
 public interface InventoryHolder {
 
     /**
      * Returns the inventory from the holder
+     *
      * @return the underling inventory
      */
-    @NotNull Inventory getInventory();
+    Inventory getInventory();
 }
