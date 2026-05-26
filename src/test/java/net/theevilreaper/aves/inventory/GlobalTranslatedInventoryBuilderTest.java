fc9e2edc27071860825f1162c18c9fea3f63c5c1
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-05-26 08:28:22 +0200

    fix(i18n): remove old translation registry and update test cases

--- src/test/java/net/theevilreaper/aves/inventory/GlobalTranslatedInventoryBuilderTest.java
+++ src/test/java/net/theevilreaper/aves/inventory/GlobalTranslatedInventoryBuilderTest.java
@@ -1,19 +1,10 @@
 package net.theevilreaper.aves.inventory;
 
-import net.theevilreaper.aves.i18n.AvesTranslationRegistry;
 import net.theevilreaper.aves.i18n.TextData;
-import net.kyori.adventure.key.Key;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
-import net.kyori.adventure.translation.GlobalTranslator;
-import net.kyori.adventure.translation.TranslationRegistry;
 import net.minestom.server.inventory.InventoryType;
-import net.minestom.testing.Env;
 import net.minestom.testing.extension.MicrotusExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.text.MessageFormat;
-import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -33,23 +24,5 @@ class GlobalTranslatedInventoryBuilderTest {
         assertNull(builder.getLayout());
         assertNull(builder.getDataLayout());
     }
-
-    @Test
-    void testGlobalTranslatedBuilderWithRegistry(Env env) {
-        TranslationRegistry translationRegistry = TranslationRegistry.create(Key.key("test", "test"));
-        translationRegistry.register("title", Locale.ENGLISH, new MessageFormat("TEST"));
-        GlobalTranslator.translator().addSource(new AvesTranslationRegistry(translationRegistry));
-
-        var builder = new GlobalTranslatedInventoryBuilder(InventoryType.CHEST_2_ROW);
-
-        builder.setTitleData(new TextData("title"));
-        builder.setLayout(InventoryLayout.fromType(builder.getType()));
-        String serialize = PlainTextComponentSerializer.plainText().serialize(builder.getInventory(Locale.ENGLISH).getTitle());
-        assertTrue(serialize.equalsIgnoreCase("TEST"));
-        assertNotNull(builder.getTitleData());
-        assertNotNull(builder.getLayout());
-        assertNull(builder.getDataLayout());
-    }
-
 
 }
