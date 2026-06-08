7479d60b6bc657631d2e6ab0dbbfbc729a74257f
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-06-08 09:37:14 +0200

    chore(map): revert record change to avoid compiling issues

--- src/main/java/net/theevilreaper/aves/map/BaseMap.java
+++ src/main/java/net/theevilreaper/aves/map/BaseMap.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * The {@link BaseMap} defines a basic structure for a map which can be used in a game context or similar.
@@ -13,10 +14,27 @@ import java.util.List;
  * It should be extended to add more values and methods to the map structure.
  *
  * @author theEvilReaper
- * @version 1.1.1
+ * @version 1.1.2
  * @since 1.0.0
  */
-public record BaseMap(String name, @Nullable Pos spawn, @Nullable List<String> builders) {
+public class BaseMap {
+
+    private final String name;
+    private final @Nullable Pos spawn;
+    private final @Nullable List<String> builders;
+
+    /**
+     * Creates a new instance of the {@link BaseMap} with the given values.
+     *
+     * @param name     of the map
+     * @param spawn    of the map
+     * @param builders of the map
+     */
+    public BaseMap(String name, @Nullable Pos spawn, @Nullable List<String> builders) {
+        this.name = name;
+        this.spawn = spawn;
+        this.builders = builders;
+    }
 
     /**
      * Creates a new instance of the {@link BaseMapBuilder} to build a new map.
@@ -48,5 +66,40 @@ public record BaseMap(String name, @Nullable Pos spawn, @Nullable List<String> b
      */
     public Pos getSpawnOrDefault(Pos defaultSpawn) {
         return spawn != null ? spawn : defaultSpawn;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public @Nullable Pos spawn() {
+        return spawn;
+    }
+
+    public @Nullable List<String> builders() {
+        return builders;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (BaseMap) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.spawn, that.spawn) &&
+                Objects.equals(this.builders, that.builders);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, spawn, builders);
+    }
+
+    @Override
+    public String toString() {
+        return "BaseMap[" +
+                "name=" + name + ", " +
+                "spawn=" + spawn + ", " +
+                "builders=" + builders + ']';
     }
 }
