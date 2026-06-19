c3059b145edbf3764d2db8c93223e1a1f9731234
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-06-19 08:50:25 +0200

    test(inventory): improve GlobalTranslatedInventoryBuilderTest

--- src/test/java/net/theevilreaper/aves/inventory/GlobalTranslatedInventoryBuilderTest.java
+++ src/test/java/net/theevilreaper/aves/inventory/GlobalTranslatedInventoryBuilderTest.java
@@ -6,23 +6,23 @@ import net.minestom.testing.extension.MicrotusExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MicrotusExtension.class)
 class GlobalTranslatedInventoryBuilderTest {
 
     @Test
     void testGlobalTranslatedBuilder() {
-
-        var builder = new GlobalTranslatedInventoryBuilder(InventoryType.CHEST_2_ROW);
+        GlobalTranslatedInventoryBuilder builder = new GlobalTranslatedInventoryBuilder(InventoryType.CHEST_2_ROW);
 
         builder.setTitleData(new TextData("title"));
 
         assertNotNull(builder.getTitleData());
         assertNull(builder.getLayout());
         assertNull(builder.getDataLayout());
-    }
 
+        assertEquals("title", builder.getTitleData().key());
+    }
 }
