2be57549210e2361e6a12689e65ff8a5f0a71852
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-07-22 16:22:17 +0200

    test(vectors): adjust angleToXAxis test case

--- src/test/java/net/theevilreaper/aves/util/VectorsTest.java
+++ src/test/java/net/theevilreaper/aves/util/VectorsTest.java
@@ -24,7 +24,9 @@ class VectorsTest {
     }
 
     @Test
-    void angleToXAxis() {
-        assertNotSame(0.7853981633974483, Vectors.angleToXAxis(Vec.ZERO));
+    void testAngleToXAxis() {
+        assertEquals(0.0, Vectors.angleToXAxis(new Vec(1, 0, 0)), 1e-6, "Vector along positive X axis should have angle 0");
+        assertEquals(Math.PI / 2, Vectors.angleToXAxis(new Vec(0, 1, 0)), 1e-6, "Vector along positive Y axis should have angle PI/2");
+        assertEquals(Math.PI / 4, Vectors.angleToXAxis(new Vec(1, 1, 0)), 1e-6, "Vector (1, 1) should have angle PI/4");
     }
 }
